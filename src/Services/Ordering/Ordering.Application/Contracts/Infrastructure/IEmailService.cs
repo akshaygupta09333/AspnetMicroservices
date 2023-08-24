3a869319dfe1d7dbfc57c6cfc403198800c6636c
Akshay Gupta <akshay.g.gupta@capgemini.com>
2023-08-24 14:51:59 +0530

    Added ordering microservice wioth cqrs

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+using Ordering.Application.Models;
+using System.Threading.Tasks;
+namespace Ordering.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
