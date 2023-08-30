ca65ce66e5ecfeebb31f8a3cf8468bb197357cd7
Akshay Gupta <akshay.g.gupta@capgemini.com>
2023-08-30 16:37:08 +0530

    Modify the Ordering microservices.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using Dapper;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository
+    {
+        private readonly IConfiguration _configuration;
+        public OrderRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            using var connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var orders = await connection.QueryFirstOrDefaultAsync<List<Order>>
+                ("SELECT * FROM Coupon WHERE UserName = @UserName", new { UserName = userName });
+            return orders;
+        }
+    }
+}
