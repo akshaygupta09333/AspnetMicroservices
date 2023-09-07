d505b6822f3e206eb16c8ce2ba559b9320c5afb0
Akshay Gupta <akshay.g.gupta@capgemini.com>
2023-09-07 14:14:32 +0530

    ocelot implementation is done.

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -2,6 +2,8 @@ using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);
+builder.Services.AddOcelot(builder.Configuration).AddCacheManager(settings => settings.WithDictionaryHandle());
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
