2b8d28d9f0f83f60d8b76525a65b12639c6b4c1c
Akshay Gupta <akshay.g.gupta@capgemini.com>
2023-09-20 18:46:16 +0530

    Added the test case for ordering microservice.

--- src/Services/Ordering/Ordering.UnitTest/OrderControllerTest.cs
+++ src/Services/Ordering/Ordering.UnitTest/OrderControllerTest.cs
@@ -1,8 +1,37 @@
 using Xunit;
 using Moq;
+using MediatR;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.API.Controllers;
+using System.Collections.Generic;
+
 namespace Ordering.UnitTest;
 
 public class OrderControllerTest
 {
+    private readonly Mock<IMediator> _mediator;
+    private readonly OrderController _controller;
+    private readonly string UserName="Akshay";
+    public OrderControllerTest()
+    {
+        _mediator = new Mock<IMediator>();
+        _controller = new OrderController(_mediator.Object);
+    }
+
+    [Fact]
+    public async Task GetOrdersByUserName_Test()
+    {
+        var result = await _controller.GetOrdersByUserName(UserName);
+        Assert.IsType<OkObjectResult>(result.Result);
+    }
+
+    [Fact]
+    public async Task DeleteOrder_Test()
+    {
+        var result = await _controller.DeleteOrder(1);
+        Assert.IsType<NoContentResult>(result);
+        _mediator.Verify();
+    }
 
 }
