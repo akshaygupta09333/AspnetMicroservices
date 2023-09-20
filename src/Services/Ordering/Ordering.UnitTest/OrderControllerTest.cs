d2a3f1e57d65a51d1dc245ed8485ccc457837374
Akshay Gupta <akshay.g.gupta@capgemini.com>
2023-09-20 18:58:27 +0530

    Completed all test cases for ordering microservices.

--- src/Services/Ordering/Ordering.UnitTest/OrderControllerTest.cs
+++ src/Services/Ordering/Ordering.UnitTest/OrderControllerTest.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Ordering.API.Controllers;
 using System.Collections.Generic;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 
 namespace Ordering.UnitTest;
 
@@ -12,7 +14,7 @@ public class OrderControllerTest
 {
     private readonly Mock<IMediator> _mediator;
     private readonly OrderController _controller;
-    private readonly string UserName="Akshay";
+    private readonly string UserName = "Akshay";
     public OrderControllerTest()
     {
         _mediator = new Mock<IMediator>();
@@ -24,6 +26,35 @@ public class OrderControllerTest
     {
         var result = await _controller.GetOrdersByUserName(UserName);
         Assert.IsType<OkObjectResult>(result.Result);
+    }
+
+    [Fact]
+    public async Task CheckoutOrder_Test()
+    {
+        CheckoutOrderCommand checkoutOrderCommand = new CheckoutOrderCommand()
+        {
+            UserName = "Akshay",
+            CardName = "Amex",
+            CardNumber = "7890765432123456",
+            CVV = "489",
+            TotalPrice = 1000
+        };
+        var result = await _controller.CheckoutOrder(checkoutOrderCommand);
+        Assert.IsType<OkObjectResult>(result.Result);
+    }
+
+    [Fact]
+    public async Task UpdateOrder_Test()
+    {
+        UpdateOrderCommand updateOrderCommand = new UpdateOrderCommand()
+        {
+            UserName = "Akshay",
+            CardName = "Amex",
+            CardNumber = "7890765432123456",
+            CVV = "489"
+        };
+        var result = await _controller.UpdateOrder(updateOrderCommand);
+        Assert.IsType<NoContentResult>(result);
     }
 
     [Fact]
